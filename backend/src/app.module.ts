7fc9a1e88c6351c7cebf9a00f22c12e15a4931ec
fatma <fatmaabdeltwab615@gmail.com>
2024-09-03 23:41:07 +0300

    'cart-wishlist-nest'

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './modules/auth/auth.module';
+import { CartModule } from './modules/cart/cart.module';
+import { WishlistModule } from './modules/wishlist/wishlist.module';
+import { BookModule } from './modules/book/book.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/onlineLibrary')],
+  imports: [AuthModule,CartModule,WishlistModule,BookModule,MongooseModule.forRoot('mongodb://localhost/nest_iti')],
   controllers: [AppController],
   providers: [AppService],
 })
