8073b4f4590cd50437e7731b5627e1e6542d8a1b
Ahmed-Gamal20 <93832798+Ahmed-Gamal20@users.noreply.github.com>
2024-09-20 02:22:34 +0300

    translation footer contact about

--- Client/src/app/pages/about/about.component.ts
+++ Client/src/app/pages/about/about.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { SubNavbarComponent } from '../../components/navbar/sub-navbar/sub-navbar.component';
 import { DarkModeService } from '../../services/dark-mode/dark-mode.service';
+import { MyTranslateService } from '../../services/translation/my-translate.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [SubNavbarComponent],
+  imports: [SubNavbarComponent, TranslateModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  constructor(private _myTranslateService:MyTranslateService){ }
+
+  changeLang(lang: string) {
+    this._myTranslateService.changLang(lang);
+  }
 }
