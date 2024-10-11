0ce03dcd8d7667c5948decb17973a5430ad83467
ElDweeek <mahmoudveteran@gmail.com>
2024-10-11 03:11:08 +0300

    api!

--- Client/src/app/util/apiUrl.ts
+++ Client/src/app/util/apiUrl.ts
@@ -1,4 +1,4 @@
-export const apiUrl = "http://localhost:3000";
+export const apiUrl = "https://andalosia-backend.vercel.app";
 export const googleClientId =
   "245458561891-1r7uviuvvqsduopobd7j656nqhvrt06r.apps.googleusercontent.com";
 
